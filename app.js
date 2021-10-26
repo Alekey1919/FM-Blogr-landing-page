40928f8127533fafddcd081f8ea0cc3da867e0e8
unknown <alejosa1919@gmail.com>
2021-10-25 23:14:54 -0300

    Deployment

---
+++ app.js
@@ -0,0 +1,113 @@
+// NAVBAR
+
+const product = document.getElementById("product");
+const company = document.getElementById("company");
+const contact = document.getElementById("contact");
+
+product.addEventListener("click", () => {
+  document
+    .getElementById("dropdown-product")
+    .classList.toggle("dropdown-active");
+
+  document
+    .getElementById("dropdown-company")
+    .classList.remove("dropdown-active");
+
+  document
+    .getElementById("dropdown-contact")
+    .classList.remove("dropdown-active");
+});
+
+company.addEventListener("click", () => {
+  document
+    .getElementById("dropdown-product")
+    .classList.remove("dropdown-active");
+
+  document
+    .getElementById("dropdown-company")
+    .classList.toggle("dropdown-active");
+
+  document
+    .getElementById("dropdown-contact")
+    .classList.remove("dropdown-active");
+});
+
+contact.addEventListener("click", () => {
+  document
+    .getElementById("dropdown-product")
+    .classList.remove("dropdown-active");
+
+  document
+    .getElementById("dropdown-company")
+    .classList.remove("dropdown-active");
+
+  document
+    .getElementById("dropdown-contact")
+    .classList.toggle("dropdown-active");
+});
+
+// MOBILE NAVBAR
+
+const mobileOpen = document.getElementById("hamburguer");
+
+mobileOpen.addEventListener("click", () => {
+  document.querySelector(".nav-mobile-container").style.left = "0vw";
+  mobileClose.style.display = "block";
+  mobileClose.style.opacity = "1";
+});
+
+const mobileClose = document.getElementById("mobile-close");
+
+mobileClose.addEventListener("click", () => {
+  document.querySelector(".nav-mobile-container").style.left = "100vw";
+  mobileClose.style.opacity = "0";
+  setTimeout(() => {
+    mobileClose.style.display = "none";
+  }, 300);
+});
+
+const mobileProduct = document.getElementById("mobile-product");
+const mobileCompany = document.getElementById("mobile-company");
+const mobileConnect = document.getElementById("mobile-connect");
+
+mobileProduct.addEventListener("click", () => {
+  document
+    .getElementById("mobile-dropdown-product")
+    .classList.toggle("mobile-dropdown-active");
+
+  document
+    .getElementById("mobile-dropdown-company")
+    .classList.remove("mobile-dropdown-active");
+
+  document
+    .getElementById("mobile-dropdown-connect")
+    .classList.remove("mobile-dropdown-active");
+});
+
+mobileCompany.addEventListener("click", () => {
+  document
+    .getElementById("mobile-dropdown-product")
+    .classList.remove("mobile-dropdown-active");
+
+  document
+    .getElementById("mobile-dropdown-company")
+    .classList.toggle("mobile-dropdown-active");
+
+  document
+    .getElementById("mobile-dropdown-connect")
+    .classList.remove("mobile-dropdown-active");
+});
+
+mobileConnect.addEventListener("click", () => {
+  document
+    .getElementById("mobile-dropdown-product")
+    .classList.remove("mobile-dropdown-active");
+
+  document
+    .getElementById("mobile-dropdown-company")
+    .classList.remove("mobile-dropdown-active");
+
+  document
+    .getElementById("mobile-dropdown-connect")
+    .classList.toggle("mobile-dropdown-active");
+});
